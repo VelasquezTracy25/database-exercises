9be4577750f37747f962a21ab1c0efc354056e10
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-11 14:51:13 -0500

    completed exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -27,7 +27,6 @@ WHERE first_name LIKE 'E%E';
 
 
 -- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
-
 SELECT CONCAT(first_name, ' ', last_name) AS name, DATEDIFF(curdate(), hire_date) as days_employed
 FROM employees
 WHERE hire_date LIKE '199%'
